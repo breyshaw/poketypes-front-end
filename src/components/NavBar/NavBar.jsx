507082a890a24950857a1f8ce37a97dbe25b3ad9
breyshaw <dawsonbrey@gmail.com>
2022-05-12 14:56:09 -0400

    remove signup from Navbar, style nav, small change to landing

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav>
+        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light border rounded border-dark border-2 bg-gradient opacity-75">
           <ul>
             <li>Welcome, {user.name}</li>
             <li><Link to="/profiles">Profiles</Link></li>
@@ -12,14 +12,36 @@ const NavBar = ({ user, handleLogout }) => {
             <li><Link to="/changePassword">Change Password</Link></li>
           </ul>
         </nav>
-      :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><NavLink to='/'>Type List</NavLink></li>
-            <li><NavLink to='/add'>Add PokeType</NavLink></li>
-          </ul>
+        :
+        <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light border rounded border-dark border-2 bg-gradient opacity-75">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top" />PokeTypes!</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+              <li className="nav-item">
+                  <Link to="/">All Types</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/login">Log In</Link>
+                </li>
+                {/* wont need a signup as I will be the only user account */}
+                {/* <li className="nav-item">
+                  <Link to="/signup">Sign Up</Link>
+                </li> */}
+                <li className="nav-item">
+                  <Link to="/add">Add PokeType (eventually Admin only)</Link>
+                </li>
+                <br />
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <h5>Hide navbar</h5>
+            </button>
+              </ul>
+            </div>
+          </div>
         </nav>
       }
     </>
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,8 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>Welcome, {user ? user.name : 'Poke-trainer'}!</h1>
+      <br />
       <h1>All Types:</h1>
     </main>
   )
