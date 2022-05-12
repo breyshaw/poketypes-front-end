3eeb60a83a4193121be5c5b7b648d29ecc75049d
breyshaw <dawsonbrey@gmail.com>
2022-05-12 15:04:58 -0400

    add navbar brand icon, add shiny to landing page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import ball from './ball.png'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
@@ -16,13 +17,13 @@ const NavBar = ({ user, handleLogout }) => {
         <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light border rounded border-dark border-2 bg-gradient opacity-75">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
-              <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top" />PokeTypes!</a>
+              <img src={ball} alt="" height="24" className="d-inline-block align-text-top" />PokeTypes!</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-center">
-              <li className="nav-item">
+                <li className="nav-item">
                   <Link to="/">All Types</Link>
                 </li>
                 <li className="nav-item">
@@ -37,8 +38,8 @@ const NavBar = ({ user, handleLogout }) => {
                 </li>
                 <br />
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <h5>Hide navbar</h5>
-            </button>
+                  <h5>Hide navbar</h5>
+                </button>
               </ul>
             </div>
           </div>
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import styles from './Landing.module.css'
+import char from './char.png'
 
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <h1>Welcome, {user ? user.name : 'Poke-trainer'}!</h1>
+      <img src={char} alt="Char" />
       <br />
       <h1>All Types:</h1>
     </main>
